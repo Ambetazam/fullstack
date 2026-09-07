bcb10020a27520b69af700185ce33ad44a7f6cf0
Ambetazam <ambetazam@gmail.com>
2026-09-07 19:17:50 +0200

    Update: I create the Inventario.java file and call the scanner via import java util

---
+++ level1/LogicDeveloping/classRoom/exercices/week8/Inventario.java
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+public class Inventario{
+
+	public static void main(String[] args) {
+
+	// 1. Declaracion del scanner en el main 'it allows me to run the scanner and callit whetever place'
+
+	Scanner reader = new Scanner(System.in);
+
+	}
+
+
+}
