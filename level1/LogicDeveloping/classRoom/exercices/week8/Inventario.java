1cf45945aa47f1cffe841d3774f75a140d4610d9
Ambetazam <ambetazam@gmail.com>
2026-09-07 23:25:30 +0200

    Update: I create the array for the Inventario.java

--- level1/LogicDeveloping/classRoom/exercices/week8/Inventario.java
+++ level1/LogicDeveloping/classRoom/exercices/week8/Inventario.java
@@ -1,14 +1,26 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class Inventario{
 
 	public static void main(String[] args) {
 
-	// 1. Declaracion del scanner en el main 'it allows me to run the scanner and callit whetever place'
+	// 1. Declaracion del scanner en el main 'it allows me to run the scanner and callit whetever place'.
 
 	Scanner reader = new Scanner(System.in);
+	
+	// 2. Declaracion de un array de 5 campos.
+	String[] cookies = new String[4];
+	cookies[0]= "Ducales";
+	cookies[1]= "Festival";
+	cookies[2]= "Tosh";
+	cookies[3]= null;
+	cookies[4]= null;
+
 
 	}
+
+
+
 
 
 }
