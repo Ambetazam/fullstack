387162af3bd8d316548b462a73030d23a1a49d51
ambet <ambetazam@gmail.com>
2026-09-15 13:22:03 -0400

    Update: discount.js with conditionals

--- level2/frontend1/week2/discount.js
+++ level2/frontend1/week2/discount.js
@@ -1,6 +1,15 @@
 let totalShop = parseFloat(prompt(`Input the total value: `));
 
-if (totalShop > 100000){
+if (totalShop > 100000 && ){
   let discount = totalShop * 0.7;
   console.log(`The total amount to pay including the discount for this purchase is: ${discount}`);
+  alert(`Thank you for your purchase you need to pay: $ ${discount} usd`)
+} else {
+  if (totalShop >= 50000 <= 60000){
+    let discount_two = totalShop * 0.4;
+    console.log(`The amount to pay is: ${discount_two}`);
+    alert(`Thank you for your purchase, the amount to pay is: ${discount_two} usd`)
+
+  }
 }
+
