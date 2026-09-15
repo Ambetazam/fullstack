9f0cacb10bfe01a17ce39a06a34cbd60d4c93205
ambet <ambetazam@gmail.com>
2026-09-15 11:19:34 -0400

    Update: conditionals on main.js

---
+++ level2/frontend1/week2/main.js
@@ -0,0 +1,11 @@
+let age = 10;
+let age2 = 15;
+
+
+alert(`The age is ${age} years old`)
+
+let answer = age > age2
+console.log(answer);
+
+answer = age < age2 || age === age2 // triple = sign to make compare if it is equals
+console.log(answer);
