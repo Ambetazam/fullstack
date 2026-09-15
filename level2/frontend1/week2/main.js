00370723430df8986f596cd27b08a35c17238f23
ambet <ambetazam@gmail.com>
2026-09-15 11:32:55 -0400

    Update: main.js with parseInt prompt

--- level2/frontend1/week2/main.js
+++ level2/frontend1/week2/main.js
@@ -1,6 +1,6 @@
 let age = 10;
 let age2 = 15;
-
+let hasEntry = true;
 
 alert(`The age is ${age} years old`)
 
@@ -9,3 +9,22 @@ console.log(answer);
 
 answer = age < age2 || age === age2 // triple = sign to make compare if it is equals
 console.log(answer);
+
+// votation system
+
+const MIN_AGE = 18;
+const NATIONALITY = "Colombia";
+
+let userAge;
+let userNationality;
+
+
+userNationality = prompt("Input your age: ");
+userAge = parseInt(prompt("Input your country: "));
+
+
+
+
+
+
+
