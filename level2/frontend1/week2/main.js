b4ca8c658f2d28e75cfdbd0af2b91c7d963720b1
ambet <ambetazam@gmail.com>
2026-09-15 12:48:37 -0400

    Update: main.js with basic votation system

--- level2/frontend1/week2/main.js
+++ level2/frontend1/week2/main.js
@@ -1,19 +1,20 @@
+/***
 let age = 10;
 let age2 = 15;
 let hasEntry = true;
 
 alert(`The age is ${age} years old`)
 
-let answer = age > age2
-console.log(answer);
+let answer_one = age > age2;
+console.log(answer_one);
 
-answer = age < age2 || age === age2 // triple = sign to make compare if it is equals
-console.log(answer);
-
+answer_two = age < age2 || age === age2;// triple = sign to make compare if it is equals
+console.log(answer_two);
+***/
 // votation system
 
 const MIN_AGE = 18;
-const NATIONALITY = "Colombia";
+const NATIONALITY = "Colombia"; //I changed to 'colombia' to change the user input to lower case.
 
 let userAge;
 let userNationality;
@@ -21,6 +22,37 @@ let userNationality;
 
 userNationality = prompt("Input your age: ");
 userAge = parseInt(prompt("Input your country: "));
+
+
+let answer = userAge >= MIN_AGE && userNationality === NATIONALITY;
+console.log(`You can vote: ${answer}`);
+
+let other_answer = MIN_AGE > userAge || NATIONALITY != userNationality;
+console.log(`Youn can vote: ${other_answer} `);
+
+/***
+if(userAge >= MIN_AGE){
+  userNationality = prompt(`What's your nationality?: `);
+  if(userNationality === NATIONALITY){
+    console.log(`You can vote`);
+  } else {
+    let citizen = prompt(`You'r Colombian`);
+    if(citizen === 'yes'){
+      console.log(`You can vote`);
+    } else {
+      console.log(`You can't vote`);
+    }
+
+}
+
+***/
+
+
+
+
+
+
+
 
 
 
