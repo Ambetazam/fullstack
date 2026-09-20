89628ca74a4e9f1ea69098056dfec81d1dc9360e
Ambetazam <ambetazam@gmail.com>
2026-09-20 21:03:39 +0200

    Update: I added the break rule to stop the console when it received 3 times log in failed

--- level2/frontend1/week3/assessment.js
+++ level2/frontend1/week3/assessment.js
@@ -8,11 +8,13 @@ let wrong = 2;
 function attemps(){
 	while ( wrong < 0 && user_one != user_input && password_user_one != password_input) {
 		console.log(`Incorrect User or Password:\nPlease try again\n${wrong} attemps left!`);
-		if (wrong === 0){
-			console.log = (`Session blocked by too many failed attemps!\n`);
-			alert(Your current session token has been loocked);
-			break;
-		}
-		 wrong = wrong - wrong;
-	}
-}
+		wrong = wrong - wrong;
+	};
+};
+
+if(wrong === 0){
+	console.log(`You have ${wrong} attemps to log in.\nYour current session has been loocked!`);
+	break;
+};
+
+while (attemps = false){};
