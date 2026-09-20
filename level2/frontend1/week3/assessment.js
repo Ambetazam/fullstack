750cfb3a6ec86b7fa6b87a70e7304848bf2dcf5c
Ambetazam <ambetazam@gmail.com>
2026-09-20 20:55:53 +0200

    Update: I added to the function 'intentos' the user messagess warnings and changed its name to 'attemps'.

--- level2/frontend1/week3/assessment.js
+++ level2/frontend1/week3/assessment.js
@@ -5,10 +5,14 @@ let password_input;
 
 let wrong = 2;
 
-function intentos(){
+function attemps(){
 	while ( wrong < 0 && user_one != user_input && password_user_one != password_input) {
-		 alert('Your current session token has been loocked');
-		 console.log = ('Session blocked by too many failed tries');
+		console.log(`Incorrect User or Password:\nPlease try again\n${wrong} attemps left!`);
+		if (wrong === 0){
+			console.log = (`Session blocked by too many failed attemps!\n`);
+			alert(Your current session token has been loocked);
+			break;
+		}
 		 wrong = wrong - wrong;
 	}
 }
