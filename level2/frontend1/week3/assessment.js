83316df49a5151196893c39424696157c55c9b9b
Ambetazam <ambetazam@gmail.com>
2026-09-20 20:15:45 +0200

    Create: Assessment Directory for the first time assessment on fronted 1 course.

---
+++ level2/frontend1/week3/assessment.js
@@ -0,0 +1,9 @@
+let user_one;
+let password_user_one;
+let user_input;
+let password_input;
+
+while (user_one != user_input && password_user_one != password_input) {
+  prompt();
+  console.log = (); 
+}
