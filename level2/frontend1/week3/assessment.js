562ff73f43a4f5fd072fd50054df7253de94fac5
Ambetazam <ambetazam@gmail.com>
2026-09-20 20:43:59 +0200

    Update: I create the 'intentos' function to count the tries did by the user and block the current session.

--- level2/frontend1/week3/assessment.js
+++ level2/frontend1/week3/assessment.js
@@ -3,7 +3,12 @@ let password_user_one;
 let user_input;
 let password_input;
 
-while (user_one != user_input && password_user_one != password_input) {
-  prompt();
-  console.log = (); 
+let wrong = 2;
+
+function intentos(){
+	while ( wrong < 0 && user_one != user_input && password_user_one != password_input) {
+		 alert('Your current session token has been loocked');
+		 console.log = ('Session blocked by too many failed tries');
+		 wrong = wrong - wrong;
+	}
 }
