e836babb70f08a0f0d6f8381d96865af002980ad
Ambetazam <ambetazam@gmail.com>
2026-09-20 22:21:25 +0200

    fixing: There some errors when I tested over with the break rule; so I commented over to see what it happens

--- level2/frontend1/week3/assessment/login.js
+++ level2/frontend1/week3/assessment/login.js
@@ -1,5 +1,5 @@
-const user_one = normalUser;
-const password_user_one = simple1234;
+const user_one = 'normalUser';
+const password_user_one = 'simple1234';
 
 let initMessage = "Wellcome to our system!\nPlease Insert your user and password:\n";
 let user_input = prompt('Insert your User:\n');
@@ -10,15 +10,20 @@ let wrong = 2;
 attemps = true;
 
 function attemps(user_one,password_user_one){
-	while ( wrong < 0 && user_one != user_input && password_user_one != password_input) {
-		console.log(`Incorrect User or Password:\nPlease try again\n${wrong} attemps left!`);
-		wrong = wrong - wrong;
+	while ( wrong < 0 && user_one != user_input || password_user_one != password_input) {
+	wrong = wrong - wrong;
 	};
 };
 
-if(wrong === 0){
-	console.log(`You have ${wrong} attemps to log in.\nYour current session has been loocked!`);
-	break;
-};
+function messages(wrong,attemps){
+	if(wrong <= 0){
+
+		console.log(`You have ${wrong} attemps to log in.\nYour current session has been loocked!`);
+		//break;
+	}else{
+		console.log(`Incorrect User or Password:\nPlease try again\n${wrong} attemps left!`);
+		break;
+	};
+}
 
 while (attemps = false){};
